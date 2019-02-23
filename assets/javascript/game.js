d1d0d3f6ff6a3c6ee36201ef9cbe232e18261819
Taylor <Taylor@taylors-mbp-2.attlocal.net>
2019-02-23 18:04:28 -0500

    all working except user can select the same letter multiple times

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,31 @@
 
-  // Creates an array that lists out all of the options (Rock, Paper, or Scissors).
+  window.onload = function() {
+    // Creating an Array to hold the options that the computer can guess
   var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-  // Creating variables to hold the number of wins, losses, and ties. They start at 0.
+  // Creates variable to dolf wins, losses, and guesses remaining
   var wins = 0;
   var losses = 0;
-  var ties = 0;
+  // var ties = 0;
   var guessesLeft = 10;
-  var usedLetter = []
-
-  // Create variables that hold references to the places in the HTML where we want to display things.
+  // create an array to hold the users guesses so the guesses can be seen (using later code)
+  var usedLetter = [];
+  //attemping to hold the computers letter in an array. still working on how to push the letter in the array. Putting it in the array would allow me to get on letter for the 10 guesses and then randomly find another letter
+  var computerLetterGuess = [];
+  // was using individual lines within the if/else statements, decided to make it into a function within a variable to make it easier when writing long code. basically restarts the game, with 10 guesses and clears the users previous selected letters
+  // 2.0 with the code was rewriten the restart function 
+  var restart = function(){
+    guessesLeft = 10;
+    usedLetter = [];
+    computerLetterGuess= [];
+    // randomComputerGuess();
+    game();}
+  function randomComputerGuess () {
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    computerLetterGuess.push(computerGuess)
+    return computerGuess}
+  
+  //creates variable that reference Ids within the html to use in the future to display information
   var directionsText = document.getElementById("directions-text");
   var userChoiceText = document.getElementById("userchoice-text");
   var computerChoiceText = document.getElementById("computerchoice-text");
@@ -19,62 +35,86 @@
   var tiesText = document.getElementById("ties-text");
   var usedLettersText = document.getElementById("used-letters-text");
   
+ 
 
-  // This function is run whenever the user presses a key.
-  document.onkeyup = function(event) {
+  // scripted this section out as it did not work as i wanted and broke the game. I was attempting to get the computer guess to be selected on load instead of on each key stroke. it did not work
+  // document.ready = function() {
+  //   var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+  //   computerLetterGuess.push(computerGuess)
+  // }
 
-    // Will change the number of guesses on each key stroke
-    // var guessesLeft = event.key
+  
+    function game () {
+      
+      var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    computerLetterGuess.push(computerGuess);
+    console.log(computerGuess[0]);
 
-    // Determines which key was pressed.
-    var userGuess = event.key;
-
-    // Randomly chooses a choice from the options array. This is the Computer's guess.
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
-    // Reworked our code from last step to use "else if" instead of lots of if statements.
-
-    // This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
-    // for(i==0)
-    if ((userGuess === "a") || (userGuess === "b") || (userGuess === "c") || (userGuess === "d") || (userGuess === "e") || (userGuess === "f") || (userGuess === "g") || (userGuess === "h") || (userGuess === "i") || (userGuess === "j") || (userGuess === "k") || (userGuess === "l") || (userGuess === "m") || (userGuess === "n") || (userGuess === "o") || (userGuess === "p") || (userGuess === "q") || (userGuess === "r") || (userGuess === "s") || (userGuess === "t") || (userGuess === "u") || (userGuess === "v") || (userGuess === "w") || (userGuess === "x") || (userGuess === "y") || (userGuess === "z")
-    ) {
-
-
+    document.onkeyup = function() {
+    
+      //this is where the users guess is selected on each key stroke
+      var userGuess = event.key;
+      
+      // for( i==0 ; i<computerChoices.length ; i++ ){
+      //where the computer guess is chosen and is alos selected on each keystroke, still trying to figure that out
+      // var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+      // computerLetterGuess.push(computerGuess)
+  
+      // forces the user to select a letter, if a symbol or number is chosen an alert will come up with code written later
+      if ((userGuess === "a") || (userGuess === "b") || (userGuess === "c") || (userGuess === "d") || (userGuess === "e") || (userGuess === "f") || (userGuess === "g") || (userGuess === "h") || (userGuess === "i") || (userGuess === "j") || (userGuess === "k") || (userGuess === "l") || (userGuess === "m") || (userGuess === "n") || (userGuess === "o") || (userGuess === "p") || (userGuess === "q") || (userGuess === "r") || (userGuess === "s") || (userGuess === "t") || (userGuess === "u") || (userGuess === "v") || (userGuess === "w") || (userGuess === "x") || (userGuess === "y") || (userGuess === "z")
+      ) {
+  
+        //if the user guess matches the the computer selection the user WINs and the game restarts adding a 1 win, scripted out the ten guesses as I ended up going with the restart function instead
        if (userGuess === computerGuess) {
         wins++;
+        // restart();
         guessesLeft = 10;
+        usedLetter = [];
+        computerLetterGuess= [];
+        game();
         }
+  
+        //if the user goes through all 10 guesses with out guessing the computers letter the user loses, adding 1 loss, and restarting the game. scripted out the code that i made useless with the restart function
         else {
         guessesLeft--;
-        usedLetter = [];
         usedLetter.push(userGuess);
         }
         if(guessesLeft === 0){
         losses++;
+        // restart()
         guessesLeft = 10;
+        usedLetter = [];
+        computerLetterGuess= [];
+        game();
         }
-
-
-         
-
-        
-      // if {
-            // this pushes the players incorrect guess to the usedArray and writes it to the HTML
-            // usedLetters.push(userGuess);
-            // document.getElementById('userchoice-text') = usedLetter;
-            // console.log(usedLetter);}
+    
   
+        // this hides the directions once a key as been used
+        directionsText.textContent = "";
+  
+  
+  
+        // This is where the users wins, loses, guesses left, and letters guessed are displayed, currently have the computers letter displayed for trouble shooting this will be removed if the game where to be published
+        userChoiceText.textContent = "You chose: " + userGuess;
+        // computerChoiceText.textContent = "The computer chose: " + computerGuess;
+        guessesLeftText.textContent = "guesses left "+ guessesLeft;
+        winsText.textContent = "wins: " + wins;
+        lossesText.textContent = "losses: " + losses;
+        // tiesText.textContent = "ties: " + ties;
+        usedLettersText.textContent = "used letters " + usedLetter;
+      }
+  
+      //an alert if the user guesses anything other than a letter
+      else (
+        alert("A Letter... Come on man!")
+      )
+    };  
 
-      // Hide the directions
-      directionsText.textContent = "";
-
-      // Display the user and computer guesses, and wins/losses/ties.
-      userChoiceText.textContent = "You chose: " + userGuess;
-      computerChoiceText.textContent = "The computer chose: " + computerGuess;
-      guessesLeftText.textContent = "guesses left "+ guessesLeft;
-      winsText.textContent = "wins: " + wins;
-      lossesText.textContent = "losses: " + losses;
-      tiesText.textContent = "ties: " + ties;
-      usedLettersText.textContent = "used letters " + usedLetter;
     }
-  };
+  game();  
+    
+  }
+
+
+  // using this as the start of my game as it is the only was to get the game to work although it does not work as intended as the computer guess is choosen on each key stroke.
+  
